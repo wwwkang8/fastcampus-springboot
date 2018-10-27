e554d0bab54439e97d208062d20addca559f717a
JeongHo <wwwkang8@gmail.com>
2018-10-27 16:06:27 +0900

    UserDetailService 추가

--- myshop/src/main/resources/data.sql
+++ myshop/src/main/resources/data.sql
@@ -14,8 +14,9 @@ values(3, 'han', 'hello title 3', 'content 3', 0, now(),3);
 insert into member_role (id, name) values (1,'USER');
 insert into member_role (id,name) values (2, 'ADMIN');
 
-insert into member(id, name, email, password, reg_date) values (1, 'kim', 'wwwkang8@gmail.com', '1234', now());
-insert into member(id, name, email, password, reg_date) values (2, 'kang', 'wwwkang9@gmail.com', '1234', now());
+insert into member(id, name, email, password, reg_date) values (1, 'kim', 'urstory@gmail.com', '{bcrypt}$2a$10$G5E3.paSJfHL1iG/Ccy.meLL2eu4I1S22nqqpShxYYU1QnNFzkiP2', now());
+
+insert into member(id, name, email, password, reg_date) values (2, 'kang', 'carami@gmail.com', '{bcrypt}$2a$10$G5E3.paSJfHL1iG/Ccy.meLL2eu4I1S22nqqpShxYYU1QnNFzkiP2', now());
 
 insert into member_member_role(member_id, member_role_id) values (1,1);
 insert into member_member_role(member_id, member_role_id) values (1,2);
